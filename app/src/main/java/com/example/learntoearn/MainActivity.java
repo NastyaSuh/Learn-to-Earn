5b341d3dc77d214f5b0379693c948ee060aebbbf
NastyaSuh <58974037+NastyaSuh@users.noreply.github.com>
2020-03-29 18:57:51 +0200

    learn-to-earn

---
+++ app/src/main/java/com/example/learntoearn/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.learntoearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.learntoearn.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
