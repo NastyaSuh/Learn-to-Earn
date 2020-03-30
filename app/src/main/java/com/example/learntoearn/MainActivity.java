bb60ccb2511f4c0aa28c062244e98018b1143bd8
NastyaSuh <58974037+NastyaSuh@users.noreply.github.com>
2020-03-30 17:00:42 +0200

    MainActivity.java

--- app/src/main/java/com/example/learntoearn/MainActivity.java
+++ app/src/main/java/com/example/learntoearn/MainActivity.java
@@ -2,15 +2,42 @@ package com.example.learntoearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.learntoearn.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openaccount();
+            }
+        });
+
+        button1 = (Button) findViewById(R.id.button_signup);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openaccount();
+            }
+        });
     }
+
+    public void openaccount(){
+        Intent intent = new Intent(this, account.class);
+        startActivity(intent);
+    }
+
 }
