41fdc6147863108e2ece48d98dd5ad7ce5283620
NastyaSuh <58974037+NastyaSuh@users.noreply.github.com>
2020-05-18 21:18:56 +0200

    new

--- app/src/main/java/com/example/learntoearn/Profile.java
+++ app/src/main/java/com/example/learntoearn/Profile.java
@@ -2,18 +2,36 @@ package com.example.learntoearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
 public class Profile extends AppCompatActivity {
     private ImageView homepage;
-
     private ImageView settingstool;
+    private ImageView photofromgallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,20 +39,21 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         homepage = findViewById(R.id.home_page);
         settingstool = findViewById(R.id.setting_tool);
-         homepage.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent intent = new Intent(Profile.this, Profile.class);
-                 startActivity(intent);
-             }
-         });
-         settingstool.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 Intent intent = new Intent(Profile.this, account.class);
-                 startActivity(intent);
-             }
-         });
-    }
+        photofromgallery = findViewById(R.id.photofromgallery);
+        homepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile.this, Profile.class);
+                startActivity(intent);
+            }
+        });
+        settingstool.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile.this, account.class);
+                startActivity(intent);
+            }
+        });
 
+    }
 }
--- app/src/main/java/com/example/learntoearn/account.java
+++ app/src/main/java/com/example/learntoearn/account.java
@@ -49,84 +49,18 @@ import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.io.IOException;
 
-/**
- * Created by Susha Nastya on 05/05/2020.
- */
-
-public class account extends AppCompatActivity implements View.OnClickListener{
+public class account extends AppCompatActivity{
     private FirebaseAuth mAuth;
-    private TextView textView_emailname;
-    private DatabaseReference databaseReference;
-    private static final String TAG = account.class.getSimpleName();
-    Button btnsave;
-    private EditText editTextName;
-    private EditText editTextSurname;
-    private ImageView profileImageView;
-    public static final int PICK_IMAGE = 100;
     private TextView profile_settings;
-    private TextView education_settings;
-    private FirebaseStorage firebaseStorage;
-    Uri imagePath;
-    private StorageReference storageReference;
     private ImageView home_page;
-
-    public account() {
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null) {
-            imagePath = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imagePath);
-                profileImageView.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
         mAuth = FirebaseAuth.getInstance();
-        if (mAuth.getCurrentUser() == null) {
-            finish();
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        }
-        storageReference = firebaseStorage.getReference();
-        firebaseStorage = FirebaseStorage.getInstance();
-        databaseReference = FirebaseDatabase.getInstance().getReference();
-        editTextName = findViewById(R.id.EditTextName);
-        editTextSurname = findViewById(R.id.EditTextSurname);
-        btnsave = findViewById(R.id.savebtn);
-        btnsave.setOnClickListener((View.OnClickListener) this);
-        textView_emailname = findViewById(R.id.textViewEmailAdress);
-        FirebaseUser user = mAuth.getCurrentUser();
-        textView_emailname.setText(user.getEmail());
         profile_settings = findViewById(R.id.textView4);
-        education_settings = findViewById(R.id.textView5);
         home_page = findViewById(R.id.home_page);
-
-        profileImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent profileintent = new Intent();
-                profileintent.setType("image/*");
-                profileintent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(profileintent, "Выберите изображение"), PICK_IMAGE);
-            }
-        });
-
-        education_settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(account.this, Learning_process_settings.class);
-                startActivity(intent);
-            }
-        });
         home_page.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -137,83 +71,7 @@ public class account extends AppCompatActivity implements View.OnClickListener{
 
     }
 
-    private void userInformation() {
-        String name = editTextName.getText().toString().trim();
-        String surname = editTextSurname.getText().toString().trim();
-        Userinformantion userinformantion = new Userinformantion(name, surname);
-        FirebaseUser user = mAuth.getCurrentUser();
-        databaseReference.child(user.getUid()).setValue(userinformantion);
-        Toast.makeText(getApplicationContext(), "Информация о пользователе обновлена", Toast.LENGTH_LONG).show();
-    }
 
-
-    @Override
-    public void onClick(View view) {
-        if(view == btnsave){
-            if(imagePath == null){
-                Drawable drawable = this.getResources().getDrawable(R.drawable.circle);
-                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.circle);
-                //openSelectProfilePictureDialog()
-                userInformation();
-                //get User Data
-                finish();
-                startActivity(new Intent(account.this, Profile.class));
-            }
-            else{
-                userInformation();
-                sendUserData();
-                finish();
-                startActivity(new Intent(account.this, Profile.class));
-            }
-        }
-    }
-
-    private void sendUserData(){
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        // get user id from Firebase>Authentification>Users
-        DatabaseReference databaseReference = firebaseDatabase.getReference(mAuth.getUid());
-        //User id/Images/ProfilePic.jpg
-        StorageReference imageReference = storageReference.child(mAuth.getUid()).child("Images").child("Profile Pic");
-        UploadTask uploadTask = imageReference.putFile(imagePath);
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(account.this, "Ошибка в загрузке фотографии", Toast.LENGTH_LONG).show();
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                Toast.makeText(account.this, "Фотография успешно загружена", Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    public void openSelectProfilePictureDialog(){
-        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        TextView title = new TextView(this);
-        title.setText("Фотография");
-        title.setPadding(10,10,10,10);
-        title.setGravity(Gravity.CENTER);
-        title.setTextColor(Color.BLACK);
-        title.setTextSize(20);
-        alertDialog.setCustomTitle(title);
-        TextView msg = new TextView(this);
-        msg.setText("Выберите фотографию для аватарки;)");
-        msg.setGravity(Gravity.CENTER_HORIZONTAL);
-        msg.setTextColor(Color.BLACK);
-        alertDialog.setView(msg);
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL,"OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {}
-        });
-        new Dialog(getApplicationContext());
-        alertDialog.show();
-        final Button okBT = alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
-        LinearLayout.LayoutParams neutralBtnLP = (LinearLayout.LayoutParams) okBT.getLayoutParams();
-        neutralBtnLP.gravity = Gravity.FILL_HORIZONTAL;
-        okBT.setPadding(10,10,10,10);
-        okBT.setTextColor(Color.BLUE);
-        okBT.setLayoutParams(neutralBtnLP);
-    }
 }
 
 
